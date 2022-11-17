03d8882c0cd46188c8b2b1d17132df4440c9908e
DaniinXorchenabo <rkbcu@mail.ru>
2022-11-17 20:55:15 +0300

    :bug: Теперь контуры треугольников снова работают!

--- lab_3/src/main.rs
+++ lab_3/src/main.rs
@@ -324,9 +324,6 @@ layout(location = 22) in mat4 move_matrix;
 
 
 void main() {
-    // float c_x = position[gl_VertexIndex % 3][0];
-    // float c_y = position[gl_VertexIndex % 3][1];
-
 
     float x_mn = float(WIGHT) / float(HEIGHT);
     float y_mn = float(HEIGHT) / float(WIGHT);
@@ -335,20 +332,6 @@ void main() {
     y_mn = max(1.0, y_mn);
     float z_mn = sqrt(x_mn * y_mn);
     z_mn = max(x_mn, y_mn);
-    // x_mn = 1.0;
-    // y_mn = 1.0;
-
-    // vec4 pos0 = vec4(position[0][0] / x_mn, position[0][1] / y_mn, position[0][2], position[0][3]) * move_matrix ;
-    // vec4 pos1 = vec4(position[1][0] / x_mn, position[1][1] / y_mn, position[1][2], position[2][3]) * move_matrix ;
-    // vec4 pos2 = vec4(position[2][0] / x_mn, position[2][1] / y_mn, position[2][2], position[3][3]) * move_matrix;
-    // mat4 poses =  position * mat4(
-    //     1./x_mn, 0.,        0.,      0.,
-    //     0.,      1./ y_mn,  0.,      0.,
-    //     0.,      0.,        1./z_mn, 0.,
-    //     0.,      0.,        0.,      1.
-    // );
-
-    // vec4 pos_m =  gl_VertexIndex % 3 > 1 ? pos2: (gl_VertexIndex % 3 < 1 ? pos0: pos1);
 
 
     mat4 poses = matrixCompMult(position * move_matrix,  mat4(
@@ -357,129 +340,61 @@ void main() {
         1.0 / x_mn, 1.0 / y_mn, 1.,     1.,
         1.0,        1.0,        1.,     1.
     ))  ;
-    // mat4 pos_m =  poses;
-    // pos_m = position * move_matrix;
-    // // pos_m = position;
-    // mat4 disp_pos_m = pos_m;
-    // mat4 disp_pos_m = pos_m * mat4(
-    //     1., 0., 0., 0.,
-    //     0., 1., 0., 0.,
-    //     0., 0., 1., 0.,
-    //     0., 0., 0., 1.
-    // );
-    // vec4 pos_m = vec4(
-    //     position[gl_VertexIndex % 3][0] * (1. / x_mn) ,
-    //     position[gl_VertexIndex % 3][1] * (1. / y_mn) ,
-    //     position[gl_VertexIndex % 3][2],
-    //     position[gl_VertexIndex % 3][3]
-    // )  * move_matrix ;
+
     mat4 _pos_m = ((move_matrix * mat4(
         1. / x_mn,  0.,         0.,         0.,
         0.,         1. / y_mn,  0.,         0.,
         0.,         0.,         1./z_mn,    0.,
         0.,         0.,         0.,         1.
     )) * position);
-    // _pos_m[0] = _pos_m[0] / _pos_m[0].w;
-    // _pos_m[1] = _pos_m[1] / _pos_m[1].w;
-    // _pos_m[2] = _pos_m[2] / _pos_m[2].w;
-    // _pos_m[3] = _pos_m[3] / _pos_m[3].w;
 
     vec4 pos_m = _pos_m[gl_VertexIndex % 3];
-    // vec4 pos_m = vec4(
-    //     _pos_m[0][gl_VertexIndex % 3],
-    //     _pos_m[1][gl_VertexIndex % 3],
-    //     _pos_m[2][gl_VertexIndex % 3],
-    //     _pos_m[3][gl_VertexIndex % 3]
-    // );
-    // vec4 pos_m = vec4(
-    //     _pos_m[gl_VertexIndex % 3][0],
-    //     _pos_m[gl_VertexIndex % 3][1],
-    //     _pos_m[gl_VertexIndex % 3][2],
-    //     _pos_m[gl_VertexIndex % 3][3]
-    // );
-    vec4 disp_pos_m =  ( mat4(
+    mat4 disp_pos_m =  ( mat4(
         0.707,  -0.408, 0., 0.,
         0.,     0.816,  0., 0.,
         -0.707, -0.408, 0., 0.,
         0.,     0.,     0., 1.
-    ) * _pos_m)[gl_VertexIndex % 3];
-    // vec3 pos_m = vec3(c_x, c_y, 1.0)  * move_matrix ;
-    //
-    // vec3 pos0 = vec3(position[0].x, position[0].y, 1.0) * move_matrix ;
-    // vec3 pos1 = vec3(position[1].x, position[1].y, 1.0) * move_matrix ;
-    // vec3 pos2 =  vec3(position[2].x, position[2].y, 1.0) * move_matrix;
+    ) * _pos_m);
 
-    // c_x = pos_m.x;
-    // c_y = pos_m.y;
+    gl_Position = vec4(disp_pos_m[gl_VertexIndex % 3].xyzw );
 
-    // points = [
-    //             [((position[0].x + 1) / 2) * WIGHT, ((1 * position[0].y + 1) / 2) * HEIGHT, 1.0],
-    //             [((position[1].x + 1) / 2) * WIGHT, ((1 * position[1].y + 1) / 2) * HEIGHT, 1.0],
-    //             [((position[2].x + 1) / 2) * WIGHT, ((1 * position[2].y + 1) / 2) * HEIGHT, 1.0]
-    //         ];
 
-    // points[ 0 ][0] = ((pos0.x + 1.0) / 2.0) * float(WIGHT) ;
-    // points[ 0 ][1] = ((1.0 * pos0.y + 1.0) / 2.0) * float(HEIGHT);
-    // points[ 0 ][2] = ((pos0.z + 1.0) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
-    // points[ 0 ][3] = 1.0;
-    //
-    // points[ 1 ][0] = ((pos1.x + 1.0) / 2.0) * float(WIGHT) ;
-    // points[ 1 ][1] = ((1.0 * pos1.y + 1.0) / 2.0) * float(HEIGHT);
-    // points[ 1 ][2] = ((pos1.z + 1) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
-    // points[ 1 ][3] = 1.0;
-    //
-    // points[ 2 ][0] = ((pos2.x + 1.0) / 2.0) * float(WIGHT) ;
-    // points[ 2 ][1] = ((1.0 * pos2.y + 1.0) / 2.0) * float(HEIGHT);
-    // points[ 2 ][2] = ((pos2.z + 1.0) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
-    // points[ 2 ][3] = 1.0;
-    //
-    // points[ 3 ][0] = 0.0;
-    // points[ 3 ][1] = 0.0;
-    // points[ 3 ][2] = 0.0;
-    // points[ 3 ][3] = 1.0;
+    points[ 0 ][0] = ((disp_pos_m[0].x + 1.0) / 2.0) * float(WIGHT) ;
+    points[ 0 ][1] = ((1.0 * disp_pos_m[0].y + 1.0) / 2.0) * float(HEIGHT);
+    points[ 0 ][2] = 0.0; //((disp_pos_m[0].z + 1.0) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
+    points[ 0 ][3] = 0.0;
 
+    points[ 1 ][0] = ((disp_pos_m[1].x + 1.0) / 2.0) * float(WIGHT) ;
+    points[ 1 ][1] = ((1.0 * disp_pos_m[1].y + 1.0) / 2.0) * float(HEIGHT);
+    points[ 1 ][2] = 0.0; //(disp_pos_m[1].z + 1) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
+    points[ 1 ][3] = 0.0;
+
+    points[ 2 ][0] = ((disp_pos_m[2].x + 1.0) / 2.0) * float(WIGHT) ;
+    points[ 2 ][1] = ((1.0 * disp_pos_m[2].y + 1.0) / 2.0) * float(HEIGHT);
+    points[ 2 ][2] = 0.0; //((disp_pos_m[2].z + 1.0) / 2.0) * sqrt(float(WIGHT) * float(HEIGHT));
+    points[ 2 ][3] = 0.0;
+
+    points[ 3 ][0] = 0.0;
+    points[ 3 ][1] = 0.0;
+    points[ 3 ][2] = 0.0;
+    points[ 3 ][3] = 1.0;
     // points =
-
-
     // points = points * move_matrix;
+    // points = mat4(
+    //     0.707,  -0.408, 0., 0.,
+    //     0.,     0.816,  0., 0.,
+    //     -0.707, -0.408, 0., 0.,
+    //     0.,     0.,     0., 1.
+    // ) * points;
 
 
-    // mat3 test_mat;
 
-    // test_mat[0][0] = 1.0;
-    // test_mat[0][1] = 0.0;
-    // test_mat[0][2] = 0.0;
-    //
-    // test_mat[1][0] = 0.0;
-    // test_mat[1][1] = 1.0;
-    // test_mat[1][2] = 0.0;
-    //
-    // test_mat[2][0] = 0.0;
-    // test_mat[2][1] = 0.0;
-    // test_mat[2][2] = 1.0;
-
-
-    // vec3 pos = test_mat * vec3(c_x, c_y, 1.0);
-
-    // gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-    // gl_Position = vec4(disp_pos_m[((gl_VertexIndex - 0) % 3) ].xyz,  1.0  );
-    // gl_Position = vec4(disp_pos_m.xy, pos_m.z, 1.0  );
-    gl_Position = vec4(disp_pos_m.xyzw );
-
-    // gl_Position = vec4(c_x, c_y, 0.0, 1.0);
     float board_size_mn =  (move_matrix[0][0] + move_matrix[1][1]) / 2;
     contour_size = vec3(
         contour[0] == 0.0 ? 0.0: (contour[0] == 1.0 ? 1.0 :max( contour[0] * length(poses[0].xyz - poses[1].xyz) / length(position[0].xy - position[1].xy), 1.4)),
         contour[1] == 0.0 ? 0.0: (contour[1] == 1.0 ? 1.0 :max( contour[1] * length(poses[1].xyz - poses[2].xyz) / length(position[1].xy - position[2].xy), 1.4)),
         contour[2] == 0.0 ? 0.0: (contour[2] == 1.0 ? 1.0 :max( contour[2] * length(poses[2].xyz - poses[0].xyz) / length(position[2].xy - position[0].xy), 1.4))
     );
-    // // contour_size = vec3(contour[0], contour[1], contour[2]);
-
-    // contour_size = vec3(contour[0] * length(pos0.xyz - pos1.xyz) / length(position[0].xy - position[1].xy),
-    //                      contour[1] * length(pos1.xyz - pos2.xyz) / length(position[1].xy - position[2].xy),
-    //                     contour[2] * length(pos2.xyz - pos0.xyz) / length(position[2].xy - position[0].xy)
-    //                     );
-
 
     contour_colors_fr = contour_colors;
 
@@ -532,39 +447,64 @@ void main() {
     // float A3 = points[2][1] - points[0][1];
     // float B3 = points[0][0] - points[2][0];
     // float C3 = points[2][0] * points[0][1] - points[0][0] * points[2][1];
+    //
+    // vec4 v_1_0 = points[1] - points[0];
+    // vec4 v_2_0 = points[2] - points[0];
+    //
+    // vec4 v_2_1 = points[2] - points[1];
+    // vec4 v_0_1 = points[0] - points[1];
+    //
+    // vec4 v_0_2 = points[0] - points[2];
+    // vec4 v_1_2 = points[1] - points[2];
+    //
+    // float A1 = v_1_0.y * v_2_0.z - v_1_0.z * v_2_0.y ;
+    // float B1 = v_1_0.z * v_2_0.x - v_1_0.x * v_2_0.z;
+    // float C1 = v_1_0.x * v_2_0.y - v_1_0.y * v_2_0.x;
+    // float D1 = (- A1 * points[0][0] - B1 * points[0][1] - C1 * points[0][2]);
+    //
+    // float A2 = v_2_1.y * v_0_1.z - v_2_1.z * v_0_1.y;
+    // float B2 = v_2_1.z * v_0_1.x - v_2_1.x * v_0_1.z;
+    // float C2 = v_2_1.x * v_0_1.y - v_2_1.y * v_0_1.x;
+    // float D2 = (- A2 * points[1][0] - B2 * points[1][1] - C2 * points[1][2]);
+    //
+    // float A3 = v_0_2.y * v_1_2.z - v_0_2.z * v_1_2.y;
+    // float B3 = v_0_2.z * v_1_2.x - v_0_2.x * v_1_2.z;
+    // float C3 = v_0_2.x * v_1_2.y - v_0_2.y * v_1_2.x;
+    // float D3 = (- A2 * points[2][0] - B2 * points[2][1] - C2 * points[2][2]);
+    //
+    //
+    // if (abs( A1 * gl_FragCoord.x   + B1 * gl_FragCoord.y + C1 * gl_FragCoord.z + D1) / sqrt(A1*A1 + B1*B1 + C1*C1) < contour_size.x )
+    //     f_color = contour_colors_fr[0];
+    //
+    // else if (abs( A2 * gl_FragCoord.x   + B2 * gl_FragCoord.y + C2 * gl_FragCoord.z + D2) / sqrt(A2*A2 + B2*B2 + C2*C2) < contour_size.y)
+    //     f_color = contour_colors_fr[1];
+    //
+    // else if (abs( A3 * gl_FragCoord.x   + B3 * gl_FragCoord.y + C3 * gl_FragCoord.z + D3) / sqrt(A3*A3 + B3*B3 + C3*C3) < contour_size.z)
+    //     f_color = contour_colors_fr[2];
+    //
+    // else
+    //     f_color = fragColor;
 
-    vec4 v_1_0 = points[1] - points[0];
-    vec4 v_2_0 = points[2] - points[0];
 
-    vec4 v_2_1 = points[2] - points[1];
-    vec4 v_0_1 = points[0] - points[1];
+    float A1 = points[0][1] - points[1][1];
+    float B1 = points[1][0] - points[0][0];
+    float C1 = points[0][0] * points[1][1] - points[1][0] * points[0][1];
 
-    vec4 v_0_2 = points[0] - points[2];
-    vec4 v_1_2 = points[1] - points[2];
+    float A2 = points[1][1] - points[2][1];
+    float B2 = points[2][0] - points[1][0];
+    float C2 = points[1][0] * points[2][1] - points[2][0] * points[1][1];
 
-    float A1 = v_1_0.y * v_2_0.z - v_1_0.z * v_2_0.y ;
-    float B1 = v_1_0.z * v_2_0.x - v_1_0.x * v_2_0.z;
-    float C1 = v_1_0.x * v_2_0.y - v_1_0.y * v_2_0.x;
-    float D1 = (- A1 * points[0][0] - B1 * points[0][1] - C1 * points[0][2]);
+    float A3 = points[2][1] - points[0][1];
+    float B3 = points[0][0] - points[2][0];
+    float C3 = points[2][0] * points[0][1] - points[0][0] * points[2][1];
 
-    float A2 = v_2_1.y * v_0_1.z - v_2_1.z * v_0_1.y;
-    float B2 = v_2_1.z * v_0_1.x - v_2_1.x * v_0_1.z;
-    float C2 = v_2_1.x * v_0_1.y - v_2_1.y * v_0_1.x;
-    float D2 = (- A2 * points[1][0] - B2 * points[1][1] - C2 * points[1][2]);
-
-    float A3 = v_0_2.y * v_1_2.z - v_0_2.z * v_1_2.y;
-    float B3 = v_0_2.z * v_1_2.x - v_0_2.x * v_1_2.z;
-    float C3 = v_0_2.x * v_1_2.y - v_0_2.y * v_1_2.x;
-    float D3 = (- A2 * points[2][0] - B2 * points[2][1] - C2 * points[2][2]);
-
-
-    if (abs( A1 * gl_FragCoord.x   + B1 * gl_FragCoord.y + C1 * gl_FragCoord.z + D1) / sqrt(A1*A1 + B1*B1 + C1*C1) < contour_size.x )
+    if (abs( A1 * gl_FragCoord.x   + B1 * gl_FragCoord.y + C1) / sqrt(A1*A1 + B1*B1) < contour_size.x )
         f_color = contour_colors_fr[0];
 
-    else if (abs( A2 * gl_FragCoord.x   + B2 * gl_FragCoord.y + C2 * gl_FragCoord.z + D2) / sqrt(A2*A2 + B2*B2 + C2*C2) < contour_size.y)
+    else if (abs( A2 * gl_FragCoord.x   + B2 * gl_FragCoord.y + C2) / sqrt(A2*A2 + B2*B2) < contour_size.y)
         f_color = contour_colors_fr[1];
 
-    else if (abs( A3 * gl_FragCoord.x   + B3 * gl_FragCoord.y + C3 * gl_FragCoord.z + D3) / sqrt(A3*A3 + B3*B3 + C3*C3) < contour_size.z)
+    else if (abs( A3 * gl_FragCoord.x   + B3 * gl_FragCoord.y + C3) / sqrt(A3*A3 + B3*B3) < contour_size.z)
         f_color = contour_colors_fr[2];
 
     else
