6d6e8fcc3915c2b0c707d11810ae7ee66b030b66
DaniinXorchenabo <rkbcu@mail.ru>
2022-11-23 15:45:36 +0300

    :building_construction: Изменил структуру репозитория (совсем)

--- src/bin/lab_7/main.rs
+++ src/bin/lab_7/main.rs
@@ -308,14 +308,14 @@ impl Figure {
 mod vs {
     vulkano_shaders::shader! {
         ty: "vertex",
-        path: "src/vertex.glsl",
+        path: "src/bin/lab_7/vertex.glsl",
     }
 }
 
 mod fs {
     vulkano_shaders::shader! {
         ty: "fragment",
-        path: "src/fragments.glsl",
+        path: "src/bin/lab_7/fragments.glsl",
         // src: ""
     }
 }
